1275a4ffe4fa76b96137cb543eeaec1ef29da567
Koen Verheyen <koen.verheyen@inuits.eu>
2021-05-21 12:42:28 +0200

    Return result from moveDeadletterJob

--- management/move-deadletter-job.js
+++ management/move-deadletter-job.js
@@ -1,7 +1,7 @@
 import flowableFetch from '../flowable-fetch'
 
 export async function moveDeadletterJob(id) {
-  await flowableFetch(`management/deadletter-jobs/${id}`, {
+  return await flowableFetch(`management/deadletter-jobs/${id}`, {
     method: 'POST',
     body: { action: 'move' },
   })
