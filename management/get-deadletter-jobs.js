cbd0313cc200db06bd7ba792f0436ce3e8e6195d
Koen Verheyen <verheyen.koen@gmail.com>
2022-09-20 11:56:31 +0200

    Added params parameter to getDeadletterJobs method

--- management/get-deadletter-jobs.js
+++ management/get-deadletter-jobs.js
@@ -1,5 +1,5 @@
 import flowableFetch from '../lib/flowable-fetch.js'
 
-export async function getDeadletterJobs() {
-  return await flowableFetch('management/deadletter-jobs')
+export async function getDeadletterJobs(params) {
+  return await flowableFetch('management/deadletter-jobs', { params })
 }
