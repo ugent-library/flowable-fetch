fe4e5ce93136b60f51c14ecb9204566fcce22492
Koen Verheyen <verheyen.koen@gmail.com>
2022-11-09 16:57:45 +0100

    Fix for getProcessInstanceVariable if variable doesn't exist

--- runtime/get-process-instance-variable.js
+++ runtime/get-process-instance-variable.js
@@ -1,7 +1,15 @@
 import flowableFetch from '../lib/flowable-fetch.js'
 
-export async function getProcessInstanceVariable(processInstanceId, variableName) {
-  const variableData = await flowableFetch(`runtime/process-instances/${processInstanceId}/variables/${variableName}`)
+export async function getProcessInstanceVariable(
+  processInstanceId,
+  variableName
+) {
+  const variableData = await flowableFetch(
+    `runtime/process-instances/${processInstanceId}/variables/${variableName}`,
+    {
+      allowedFailStatuses: [404],
+    }
+  )
 
   if (variableData) {
     return variableData.value
